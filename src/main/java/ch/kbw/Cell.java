89170219b6669ebd97959a5d7c6be0c1ea822320
Sascha Sturzenegger <sascha@sturzenegger-wyss.ch>
2022-03-25 09:16:17 +0100

    Removed unused variables and methods from Cell.java

--- src/main/java/ch/kbw/Cell.java
+++ src/main/java/ch/kbw/Cell.java
@@ -5,27 +5,12 @@ import javafx.scene.control.Label;
 
 public class Cell extends Label {
 
-
-    int wert;
     String stringwert;
     public Cell(){
         super();
     }
 
-    public Cell(int wert){
-        this.wert = wert;
-    }
-
     public Cell(String stringwert){
         this.stringwert=stringwert;
     }
-
-    public int getWert() {
-        return wert;
-    }
-
-    public void setWert(int wert) {
-        this.wert = wert;
-    }
-
 }
